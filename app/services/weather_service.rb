3b2b59485d01e4843a168235f0df22123567e4e0
Carina Sweet <carina.sweet06@gmail.com>
2021-09-24 16:23:56 -0600

    Add Weather Service and test

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,2 +1,12 @@
 class WeatherService < BaseService
+  def self.get_weather(lat, lon)
+    response = conn('https://api.openweathermap.org/data/2.5/onecall').get do |req|
+      req.params['appid'] = ENV['open_weather_key']
+      req.params['lat'] = lat
+      req.params['lon'] = lon
+      req.params['exclude'] = 'minutely,alerts'
+      req.params['units'] = 'imperial'
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
---
+++ spec/services/weather_service_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe WeatherService do
+  it 'can get lat and long for location given in params', :vcr do
+    json = WeatherService.get_weather('39.738453', '-104.984853')
+
+    expect(json).to have_key(:current)
+    expect(json[:current]).to have_key(:dt)
+    expect(json[:current]).to have_key(:sunrise)
+    expect(json[:current]).to have_key(:sunset)
+    expect(json[:current]).to have_key(:feels_like)
+    expect(json[:current]).to have_key(:uvi)
+    expect(json[:current]).to have_key(:visibility)
+    expect(json[:current]).to have_key(:uvi)
+    expect(json[:current][:weather][0]).to have_key(:description)
+    expect(json[:current][:weather][0]).to have_key(:icon)
+
+    # next five days
+    expect(json).to have_key(:daily)
+    expect(json[:daily][0]).to have_key(:dt)
+    expect(json[:daily][0]).to have_key(:sunrise)
+    expect(json[:daily][0]).to have_key(:sunset)
+    expect(json[:daily][0][:temp]).to have_key(:max)
+    expect(json[:daily][0][:temp]).to have_key(:min)
+    expect(json[:daily][0][:weather][0]).to have_key(:description)
+    expect(json[:daily][0][:weather][0]).to have_key(:icon)
+
+    #next eight hours
+    expect(json).to have_key(:hourly)
+    expect(json[:hourly][0]).to have_key(:dt)
+    expect(json[:hourly][0]).to have_key(:temp)
+    expect(json[:hourly][0][:weather][0]).to have_key(:description)
+    expect(json[:hourly][0][:weather][0]).to have_key(:icon)
+
+  end
+end
