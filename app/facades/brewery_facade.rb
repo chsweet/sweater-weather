e6e631a9ca45d406dd10ce54ff6f02c073baa2fa
Carina Sweet <carina.sweet06@gmail.com>
2021-09-28 09:30:56 -0600

    Update brewery facade

--- app/facades/brewery_facade.rb
+++ app/facades/brewery_facade.rb
@@ -1,6 +1,9 @@
 class BreweryFacade
   def self.create_breweries(lat_lng, quantity)
     json = OpenBreweryService.get_location_breweries(lat_lng, quantity)
-    brewery = Brewery.new(json)
+
+    json.map do |brewery|
+      Brewery.new(brewery)
+    end
   end
 end
--- spec/facades/brewery_facade_spec.rb
+++ spec/facades/brewery_facade_spec.rb
@@ -3,6 +3,8 @@ require 'rails_helper'
 RSpec.describe BreweryFacade do
   it 'can create breweries with lat and log and quantity', :vcr do
     breweries = BreweryFacade.create_breweries('39.738453,-104.984853', 5)
-    expect(brewery).to be_a(Brewery)
+
+    expect(breweries).to be_an(Array)
+    expect(breweries[0]).to be_a(Brewery)
   end
 end
