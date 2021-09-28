59eb68f2858aab789902f538a4ace525f0a1edab
Carina Sweet <carina.sweet06@gmail.com>
2021-09-28 10:01:10 -0600

    add breweries serializer and update controller

---
+++ app/controllers/api/v1/breweries_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::BreweriesController < ApplicationController
+  def index
+    location = LocationFacade.create_location(params[:location])
+
+    forecast = ForecastFacade.create_forecast(location.latitude, location.longitude)
+
+    breweries = BreweryFacade.create_breweries("#{location.latitude}, #{location.longitude}", params[:quantity])
+
+    render(json: BreweriesSerializer.location_forecast_breweries(params[:location], forecast, breweries))
+  end
+end
