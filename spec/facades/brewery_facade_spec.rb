0044ae7f80ab8739543ec92fef1db523c1e35a41
Carina Sweet <carina.sweet06@gmail.com>
2021-09-28 09:28:35 -0600

    Add brewery facade

--- spec/facades/brewery_facade_spec.rb
+++ spec/facades/brewery_facade_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe BreweryFacade do
   it 'can create breweries with lat and log and quantity', :vcr do
-    location = LocationFacade.create_location('Denver, CO')
-    expect(location).to be_a(Location)
+    breweries = BreweryFacade.create_breweries('39.738453,-104.984853', 5)
+    expect(brewery).to be_a(Brewery)
   end
 end
